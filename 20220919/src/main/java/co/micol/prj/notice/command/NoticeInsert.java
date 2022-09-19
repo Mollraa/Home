e2bf54b56552f97a47dc469b31cdd5c6fbdc3630
sec <sec@LAPTOP-47TM6HJ6>
2022-09-19 23:51:42 +0900

    0919..Good Night..

---
+++ 20220919/src/main/java/co/micol/prj/notice/command/NoticeInsert.java
@@ -0,0 +1,37 @@
+package co.micol.prj.notice.command;
+
+import java.sql.Date;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import co.micol.prj.common.Command;
+import co.micol.prj.notice.service.NoticeService;
+import co.micol.prj.notice.service.NoticeVO;
+import co.micol.prj.notice.serviceImpl.NoticeServiceImpl;
+
+public class NoticeInsert implements Command {
+
+	@Override
+	public String exec(HttpServletRequest request, HttpServletResponse response) {
+		// 게시글 등록
+		String viewPage = "notice/noticeError";
+		NoticeService dao = new NoticeServiceImpl();
+		NoticeVO vo = new NoticeVO();
+		vo.setNoticeWriter(request.getParameter("noticeWriter"));
+		vo.setNoticeDate(Date.valueOf(request.getParameter("noticeDate"))); //문자를 java Date객체로 변환
+		vo.setNoticeTitle(request.getParameter("noticeTitle"));
+		vo.setNoticeSubject(request.getParameter("noticeSubject"));
+		
+		//첨부파일이 있을시 이곳에서 처리함
+		int n = dao.noticeInsert(vo);
+		if(n != 0) {
+			viewPage = "noticeSelectList.do";
+		}else {
+			request.setAttribute("message", "게시글 등록이 실패했다.");
+		}
+		
+		return viewPage;
+	}
+
+}
